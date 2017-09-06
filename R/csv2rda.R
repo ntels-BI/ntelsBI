87cb1bfe1aef9e14cd46fca0c4744bf4acbab434
lovetoken <lt.lovetoken@gmail.com>
2017-09-06 13:39:45 +0900

    V0.1.51 Commit

--- R/csv2rda.R
+++ R/csv2rda.R
@@ -1,22 +1,22 @@
 #' Write rda about imported tables
 #' @description 워킹디렉토리에 존재하는 테이블 파일들을 import 한 후 rda 형태로 모두 변환합니다. 기호에 따라 변환되고 난 후 csv 파일을 지울 수 있습니다. 본 함수는 \code{readr::read_csv()} 함수에 의존성을 가지고 있습니다.
-#' @param path
+#' @param path a character
 #' @param delete
 #' @param method choice machine learning algorithm method
 #' @param fitImage a character. Input filename for chaching fit Object
 #' @return \code{rda} 포맷에서 객체의 이름은 \code{rawdata} 로 지정되어 저장됩니다.
 #' @export
 
-csv2rda <- function(path = ".", delete = F, format = c("csv", "txt"), ...){
+csv2rda <- function(pattern = NULL, delete = F, format = c("csv", "txt"), ...){
 
   ## Pre
   stopifnot(require(tidyverse))
-  path <- as.character(path)
 
   ## Content
-  impFormat <- paste0(format, collapse = "|") %>% paste0("\\.(", ., ")$")
-  files <- list.files(path)
-  impPat <- files[grep(impFormat, files, perl = T)]
+  if(is.null(pattern)){
+    impFormat <- paste0(format, collapse = "|") %>% paste0("\\.(", ., ")$")
+    impPat <- grep(impFormat, list.files(), perl = T, value = T)
+  } else impPat <- grep(pattern, list.files(), perl = T, value = T)
 
   ## Output (saving .rda format)
   for(i in impPat){
