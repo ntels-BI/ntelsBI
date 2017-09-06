e08b7e8e9d02a988a2eb320bb69cfe78f1f31f08
lovetoken <lt.lovetoken@gmail.com>
2017-09-06 10:29:27 +0900

    V0.1.42 Commit

--- R/ml.R
+++ R/ml.R
@@ -1,15 +1,16 @@
 #' Machine learning framework using caret package
-#' @description Machine learning framework using caret package.
+#' @description 예측의 대상이되는 주관심변수를 기준으로 동등비율이 되게 Training set 과 Test set 을 분리시킨 후 Training set 에 대해서만 학습시킨 결과를 list 형으로 반환합니다. 단 Test set 은 반환되는 리스트의 속성값으로 저장됩니다.
 #' @param data a data.frame.
 #' @param class a character. Input the class Y variable names
 #' @param method choice machine learning algorithm method
 #' @param fitImage a character. Input filename for chaching fit Object
+#' @return list type 이 반환되나 \code{ml} 클래스 입니다.
 #' @export
 #' @example
 #' ml(iris, "Species")
 
 ml <- function(data, class,
-               method = "rpart", partitionRate = .7, fitImage = NULL, ...){
+               method = "rpart", partitionRate = .7, tuneLength = 5, fitImage = NULL, ...){
 
   ## Pre
   stopifnot(require(tidyverse)); stopifnot(require(caret));
@@ -26,12 +27,10 @@ ml <- function(data, class,
   fitControl <- trainControl(method = "repeatedcv", ...)
 
   ## Create fit object
-  procTime <- system.time({
-    fit <- train(fm, data = training, method = method, trControl = fitControl, tuneLength = 5)
-  })
+  procTime <- system.time(fit <- train(fm, data = training, method = method, trControl = fitControl, tuneLength = tuneLength))
 
   ## Output
-  res <- list(training = training, testing = testing, class = class, fit = fit, procTime = procTime)
+  res <- list(class = class, fit = fit, testing = testing, procTime = procTime)
   class(res) <- "ml"
   if(!is.null(fitImage)) save(res, file = paste0("./output/", fitImage, ".rda"))
   return(res)
@@ -39,7 +38,7 @@ ml <- function(data, class,
 }
 
 #' Fitting summary
-#' @description Fitting assess summary, Classification 일 경우 혼돈메트릭스, Regression 일 경우 MSE 가 반환되며 \code{type} 인자를 통해 Classification 인지 Regression 인지 선택합니다.
+#' @description \code{ml} class 인 학습결과 및 테스트셋을 이용하여 Classification 일 경우 혼돈메트릭스, Regression 일 경우 MSE 가 반환되며 \code{type} 인자를 통해 Classification 인지 Regression 인지 선택합니다.
 #' @param fitObject a "ml" class object
 #' @param testset if new testset
 #' @param method choice machine learning algorithm method
@@ -57,30 +56,33 @@ fitSummary <- function(fitObject, testset = NULL, class = fitObject$class, type 
   class <- as.character(class)
 
   ## Content
-  fitRes <- fitObject$fit
+  fit <- fitObject$fit
 
-  if(type == "cla"){
+  if(type == "cla"){ # output confusion matrix
 
     if(is.null(testset)){
-      res <- confusionMatrix(predict(fitRes, newdata = fitObject$testing),
+      res <- confusionMatrix(predict(fit, newdata = fitObject$testing),
                              fitObject$testing %>% pull(class), ...)
     } else {
-      res <- confusionMatrix(predict(fitRes, newdata = testset),
+      res <- confusionMatrix(predict(fit, newdata = testset),
                              testset %>% pull(class), ...)
     }
 
-  } else if(type == "reg"){
+  } else if(type == "reg"){ # output MSE
 
     if(is.null(testset)){
-      res <- mean((predict(fitRes, newdata = fitObject$testing) - pull(fitObject$testing, class))^2)
+      res <- mean((predict(fit, newdata = fitObject$testing) - pull(fitObject$testing, class))^2)
     } else {
-      res <- mean((predict(fitRes, newdata = fitObject$testing) - pull(testset, class))^2)
+      res <- mean((predict(fit, newdata = fitObject$testing) - pull(testset, class))^2)
     }
 
   }
 
   ## Output
-  paste0("Traning set N row : ", nrow(fitObject$training), " / ", "Test set N row : ", nrow(fitObject$testing)) %>% message
   return(res)
 
 }
+
+#' @export
+
+print.ml <- function(x) print(x[c("fit", "class", "procTime")])
